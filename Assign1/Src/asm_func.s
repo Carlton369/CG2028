36ca3d9c434e192696080532d28fdb2046a5cba9
Yee Zhan Xian Carlton <carltonyxz@gmail.com>
2025-02-15 12:04:33 +0800

    added entrying cars

--- Assign1/Src/asm_func.s
+++ Assign1/Src/asm_func.s
@@ -34,7 +34,7 @@
 
 
 .equ total_sections, 6
-
+.equ max_car_per_section, 12
 asm_func:
  	PUSH {R14}
 
@@ -45,6 +45,7 @@ asm_func:
 	BX LR
 
 SUBROUTINE:
+	PUSH {R14}
 	//find total cars coming in
 	LDR R4, =total_sections
 	STR R4, [R3, #4]
@@ -58,8 +59,34 @@ add_loop:
 	SUBS R6, #1 //check how many in entry[] left
 	BNE add_loop  //checks if flag updated from prev line
 
-	STR R5, [R3, #4]
-//can reuse R6-7
+//	STR R5, [R3, #4] //print statement to check if sum working
+//can reuse R6 and R8
+	LDR R4, =total_sections //reset count
+//R1 not being used anymore
+@R5 holds total cars entering
+//now use R1 to hold val of 12
+//and use R6 to hold building
+park_cars:
+	LDR R1, =max_car_per_section
+	LDR R6, [R0] //add first num in R0
+	SUB R1, R6 //R1 now has number of cars that can fit
+	//branch here if remainder is greater than or less than entering cars
+	CMP R1, R5
+	BGT no_leftover_cars
+	//if here means section is going to be full, fill up then go to next section
+	LDR R8, =max_car_per_section
+	STR R8, [R3] //stores 12 in the RESULT SECTION
+	//INCREMENT INDEX FOR BOTH BUILDING AND RESULT
+	ADD R0, #4
+	ADD R3, #4
+
+	SUBS R5, R1 //FIND HOW MANY CARS LEFT
+	BNE park_cars
+
+no_leftover_cars:
+	//if got leftover cars, count how many leftover
 	BX LR //RETURN TO ASM_FUNC
+
+
 
 
